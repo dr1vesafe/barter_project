e242064f09bea7c3595fd230348d2b74c869eb3c
dr1vesafe <snddadaya@gmail.com>
2025-04-15 13:37:40 +0400

    Created Ad, ExchangeProposal models. Registred in admin

--- barter_project/ads/admin.py
+++ barter_project/ads/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ad, ExchangeProposal
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'description', 'category', 'condition']
+    list_filter = ['category', 'condition', 'created_at']
+    search_fields = ['title', 'description']
+
+@admin.register(ExchangeProposal)
+class ExchangeProposalAdmin(admin.ModelAdmin):
+    list_display = ['ad_sender', 'ad_receiver', 'comment', 'status']
+    list_filter = ['status', 'created_at']
+    search_fields = ['ad_sender', 'ad_receiver']
--- barter_project/ads/models.py
+++ barter_project/ads/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Ad(models.Model):
+    CATEGORY_CHOICES = [
+        ('clothes', 'Одежда'),
+        ('electronics', 'Электроника'),
+        ('jewelry', 'Бижутерия'),
+        ('furniture', 'Мебель'),
+        ('other', 'Другое'),
+    ]
+
+    CONDITION_CHOICES = [
+        ('new', 'Новый'),
+        ('used', 'Б/У'),
+    ]
+
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    image_url = models.URLField(blank=True, null=True, verbose_name='Изображение')
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, verbose_name='Категория')
+    condition = models.CharField(max_length=50, choices=CONDITION_CHOICES, verbose_name='Состояние')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.title
+
+class ExchangeProposal(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Ожидает'),
+        ('accepted', 'Принята'),
+        ('rejected', 'Отклонена'),
+    ]
+    ad_sender = models.ForeignKey(to=Ad, on_delete=models.CASCADE, related_name='sent_proposals', verbose_name='Отправитель предложения')
+    ad_receiver = models.ForeignKey(to=Ad, on_delete=models.CASCADE, related_name='received_proposals', verbose_name='Получатель предложения')
+    comment = models.TextField(verbose_name='Комментарий')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending', verbose_name='Статус')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
